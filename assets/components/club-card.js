1072a1632badd0b8652fab62bc9c769299181be1
tsolmonsq <84216429+tsolmonsq@users.noreply.github.com>
2023-12-17 03:12:09 +0800

    Update club-card.js

--- assets/components/club-card.js
+++ assets/components/club-card.js
@@ -1,69 +1,98 @@
+const template = document.createElement("template");
+
+template.innerHTML = `
+      <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+      integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+      />
+      <link rel="stylesheet" href="./assets/styles/clubs.css" />
+      
+      <article class="club-card">
+        <img class="cover"/>
+        <club-like-btn></club-like-btn>
+        <div class="card-head">
+          <img class="club-logo" />
+          <div class="card-head-txt">
+            <span class="club-tag"></span>
+            <h3></h3>
+          </div>
+        </div>
+        <p class="club-description"></p>
+        <ul class="card-status">
+          <li class="founded-year">
+            <i class="fa-solid fa-calendar-days"></i>
+            <p></p>
+          </li>
+          <li class="members">
+            <i class="fa-regular fa-user"></i>
+            <p></p>
+          </li>
+        </ul>
+      </article>
+`;
+
+
 class ClubCard extends HTMLElement {
     constructor() {
         super();
-        //implementation
         this.attachShadow({mode:'open'});
+        this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.addEventListener("club-like-btn-liked", ()=>this.#likeBtnClicked(true));
+        this.addEventListener("club-like-btn-disliked", ()=>this.#likeBtnClicked(false));
     }
-      render() {
+    #likeBtnClicked(val){
+      console.log("club-like-btn event recieved!");
+        const evnt = new CustomEvent('club-like-clicked', {
+          composed: true,
+          detail: {
+            name: this.name,
+            isLiked: val
+          }
+        });
+      
+      
+      window.dispatchEvent(evnt);
+    }
 
-        this.shadowRoot.innerHTML=   `<link rel="stylesheet" href="./assets/styles/clubs.css"/>
-        
-        
-        <article class = "club-card"> <img class="cover" src="${this.getAttribute('coverImage')}" alt=cover" />
-        <div class="card-head">
-          <img class="club-logo" src="${this.getAttribute('logo')}" alt="name" />
-          <div class="card-head-txt">
-            <span class="club-tag">${this.getAttribute('c-tag')}</span>
-            <h3>${this.getAttribute('name')}</h3>
-          </div>
-        </div>
-        <p class="club-description">${this.getAttribute('desc')}</p>
-        <ul class="card-status">
-          <li>
-            <i class="fa-solid fa-calendar-days"></i>
-            <p>${this.getAttribute('f-year')}</p>
-          </li>
-          <li>
-            <i class="fa-regular fa-user"></i>
-            <p>${this.getAttribute('members')}</p>
-          </li>
-          <li>
-            <i class="fa-regular fa-pen-to-square"></i>
-            <p>${this.getAttribute('act-status')}</p>
-          </li>
-        </ul> </article>`;
-
-      }
-        
-
-
-
-
-
-
+    connectedCallback(){
+      this.shadowRoot.getElementById("heart-button").addEventListener("click", () => {
+        console.log("Added to Cart");
+        const cart = document.querySelector("club-cart");
+        cart.AddToCart(this);
+      });
+    }
     
-
-
-
-    connectedCallback() {
-        //implementation
-
-        this.render();
+    static get observedAttributes() {
+      return ["logo", "cover", "name", "category", "desc", "fyear", "members"];
     }
 
-    disconnectedCallback() {
-        //implementation
-    }
-
-    attributeChangedCallback(name, oldVal, newVal) {
-        //implementation
-    }
-
-    adoptedCallback() {
-        //implementation
-    }
-
+    attributeChangedCallback(name, oldVal, newVal) { 
+      switch(name){
+        case "cover":
+          this.shadowRoot.querySelector(".cover").src = newVal;
+          break;
+        case "logo":
+          this.shadowRoot.querySelector(".club-logo").src = newVal;
+          break;
+        case "name":
+          this.shadowRoot.querySelector(".card-head-txt h3").innerHTML = newVal;
+          break;
+        case "category":
+          this.shadowRoot.querySelector(".club-tag").innerHTML = newVal;
+          break;
+        case "desc":
+          this.shadowRoot.querySelector(".club-description").innerHTML = newVal;
+          break;
+        case "fyear":
+          this.shadowRoot.querySelector(".founded-year p").innerHTML = newVal;
+          break;
+        case "members":
+          this.shadowRoot.querySelector(".members p").innerHTML = newVal;
+          break;
+      }
+  }
 }
-
 window.customElements.define('club-card', ClubCard);
-export default ClubCard;
